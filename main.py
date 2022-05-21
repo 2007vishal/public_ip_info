dcbf56404ff9cabfe5722f992ba50346140190ae
Mr.Nobody..! <96808346+2007vishal@users.noreply.github.com>
2022-05-21 08:17:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import whois
 import json
 import requests
 import re
+from geopy.geocoders import Nominatim
+
 
 print("+-----------------------------------------------------+")
-
 print("| 		 Developed by : vishal                |")
-
 print("|   instagram : https://instagram.com/mr__nobody__23  |")
-
 print("+-----------------------------------------------------+")
+print("") 
 regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+
  
+def self_ip():
+	import requests
+	ip = requests.get('https://api.ipify.org').text
+	return ip
+	 
 def check_ip(ip):
 	if(re.search(regex, ip)):
 		print(f"[✓] Valid IP address.")
@@ -19,8 +25,15 @@ def check_ip(ip):
 		print(f"[X] Invalid IP address.")
 		quit()
 
-ip = input("[+] Enter Public IP : ")
+#IP = self_ip()
+#print(f"[+]Your Public IP is : {IP}. \n[+] Press enter to scan your IP.")
+ip = input("[+] Enter Public IP. (press enter to scan your ip) : ")
 
+if len(ip) == 0:
+	IP = self_ip()
+	ip = IP
+	print(f"[!] Your IP is {ip} ")
+	
 check_ip(ip)
 
 try:
@@ -28,6 +41,7 @@ try:
 	url = "http://ip-api.com/json/"
 	response = requests.get(url+ip)
 	data = response.json()
+
 except:
 	print("[-] you are offline")
 	quit()
@@ -41,6 +55,13 @@ if data['status'] == 'fail':
 	
 else:
 	
+	 
+	lat = data['lat']
+	lon = data['lon']
+	a = lat , lon
+	geoLoc = Nominatim(user_agent="GetLoc")
+	locname = geoLoc.reverse(a)
+
 	print(f"IP        :  {data['query']}")
 	print(f"Status    :  {data['status']}")
 	print(f"Country   :  {data['country']}")
@@ -54,6 +75,7 @@ else:
 	print(f"latitude  :  {data['lat']}")
 	print(f"longitude :  {data['lon']}")
 	print(f"location  :  {data['lat']},{data['lon']}")
+	print(f"Address   :  {locname.address}")
 	print('\n')
 
 
